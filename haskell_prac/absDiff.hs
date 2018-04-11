20d29d9cb725abb113df157c7a895fdbb2404b69
foongminwong <wongfoongmin@hotmail.com>
2018-04-11 19:50:04 +0000

    tango abs

--- haskell_prac/absDiff.hs
+++ haskell_prac/absDiff.hs
@@ -1,3 +1,11 @@
-absDiff :: Num a => a -> a -> a
-absDiff n = abs . (n-)
+import Data.List
+import Data.Char
+import Data.Ord
+import Data.Function
+import GHC.Exts
 
+--absDiff :: Num a => a -> a -> a
+--absDiff n = abs . (n-)
+
+absDiffOrder :: Ord a => (a,a) -> a
+absDiffOrder (a,b) = abs (a-b)
--- haskell_prac/tango.hs
+++ haskell_prac/tango.hs
@@ -1,6 +1,8 @@
+import GHC.Exts
 import Data.Function
 import Data.Char
 import Data.List
+import Data.Ord
 
 tango :: Ord a => (a,a) -> a
 tango (a,b) = max a b
